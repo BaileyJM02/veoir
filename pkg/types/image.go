4e7c3ad6e1a91b0602d9ec75f398f9b6e028604e
Bailey Matthews <bailey@hey.com>
2021-02-04 14:14:43 +0000

    Improve: Change how we create images Big change - using a new package, but we can generate PNGs and SVGs via one canvas instead of SVG files. Also fixes text not showing in PNG.

--- pkg/types/image.go
+++ pkg/types/image.go
@@ -1,9 +1,14 @@
 package types
 
+import (
+	"github.com/tdewolff/canvas"
+)
+
 // Image comment
 type Image struct {
-	Payload   string   `json:"payload"`
-	Encodings []string `json:"encodings"`
-	Theme     string   `json:"theme"`
-	Hash      string   `json:"hash"`
+	Payload   string        `json:"payload"`
+	Encodings []string      `json:"encodings"`
+	Theme     string        `json:"theme"`
+	Hash      string        `json:"hash"`
+	Canvas    canvas.Canvas
 }
